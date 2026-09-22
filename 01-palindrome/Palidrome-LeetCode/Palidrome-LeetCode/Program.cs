863ac5575281e9dd8fc406a3afdea34092af4d45
Claudio Matheus <claudiomatheus055@gmail.com>
2026-09-21 22:27:51 -0300

    feat: Adiciona implementação da classe Program e método IsPalindrome

--- 01-palindrome/Palidrome-LeetCode/Palidrome-LeetCode/Program.cs
+++ 01-palindrome/Palidrome-LeetCode/Palidrome-LeetCode/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var p = new Program();
+        System.Console.WriteLine(p.IsPalindrome(121));
+    }
+
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+
+        int original = x;
+        long invertido = 0;
+
+        while (x > 0)
+        {
+            int ultimoDigito = x % 10;
+
+            invertido = invertido * 10 + ultimoDigito;
+
+            x = x / 10;
+        }
+
+        return original == invertido;
+    }
+}
