a33fc1a4ce2ebb55ea16395325e0e354c9e83377
Claudio Matheus <claudiomatheus055@gmail.com>
2026-09-21 22:38:53 -0300

    feat: Adiciona implementação da solução para remover duplicatas de um array ordenado

---
+++ 26. Remove Duplicates from Sorted Array/26.RemoveDuplicatesfromSortedArray/26.RemoveDuplicatesfromSortedArray/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var p = new Program();
+        System.Console.WriteLine(p.RemoveDuplicates(new int[] { 1, 1, 2 }));
+    }
+    public int RemoveDuplicates(int[] nums)
+    {
+        int k = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[k - 1])
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+}
