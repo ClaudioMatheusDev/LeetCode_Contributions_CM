5b80a5600b554137480e28ffb1e78a22f177aac7
Claudio Matheus <claudiomatheus055@gmail.com>
2026-09-22 22:20:44 -0300

    feat: Adiciona implementação da solução para o problema "Two Sum"

---
+++ 1.Two Sum/1. Two Sum/1. Two Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var p = new Program();
+        int[] resultado = p.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+
+        System.Console.WriteLine(string.Join(", ", resultado));
+    }
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> mapa = new Dictionary<int, int>();
+
+        int complemento = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            complemento = target - nums[i];
+            
+            if (mapa.ContainsKey(complemento))
+            {
+                return new int[] { mapa[complemento], i };
+            }
+
+            if (!mapa.ContainsKey(nums[i]))
+            {
+                mapa.Add(nums[i], i);
+            }
+        }
+
+        return new int[] { };
+    }
+
+}
