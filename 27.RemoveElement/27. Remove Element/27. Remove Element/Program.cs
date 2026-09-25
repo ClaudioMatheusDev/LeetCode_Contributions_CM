48ece581056fa78eb10ff1d6bc6d58a5861987f3
Claudio Matheus <claudiomatheus055@gmail.com>
2026-09-25 20:21:16 -0300

    feat: Program.cs com implementação da função RemoveElement e exemplo de uso no método Main.

---
+++ 27.RemoveElement/27. Remove Element/27. Remove Element/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var p = new Program();
+        int[] nums = { 3, 2, 2, 3 };
+        System.Console.WriteLine(p.RemoveElement(nums, 3));
+    }
+
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
